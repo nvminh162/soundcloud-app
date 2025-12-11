7765d51e3282c105bb337bbccabff38de9d68d68
nvminh162 <nvminh162@gmail.com>
2025-12-12 01:13:56 +0700

    feat: implement AppHeader component and update HomePage layout

--- soundcloud-nextjs-frontend/src/app/page.tsx
+++ soundcloud-nextjs-frontend/src/app/page.tsx
@@ -1,6 +1,9 @@
+import AppHeader from "@/components/header/app.header";
 
 export default function HomePage() {
   return (
-    <div>Hello world!</div>
+    <div>
+      <AppHeader />
+    </div>
   );
 }
