b04708b19576dae712fa81607cfeaabee0d63aca
nvminh162 <nvminh162@gmail.com>
2025-12-14 23:45:24 +0700

    fix: standardize import quotes and format MainSlider component in HomePage

--- soundcloud-nextjs-frontend/src/app/page.tsx
+++ soundcloud-nextjs-frontend/src/app/page.tsx
@@ -1,10 +1,23 @@
-import MainSlider from "@/components/main/main.slider";
-import { Container } from "@mui/material";
+import MainSlider from '@/components/main/main.slider';
+import { Container } from '@mui/material';
 
-export default function HomePage() {
+export default async function HomePage() {
+    const res = await fetch('http://localhost:8000/api/v1/tracks/top', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            category: 'CHILL',
+            limit: 10,
+        }),
+    });
+
+    console.log(await res.json());
+
     return (
         <Container>
-            <MainSlider/>
+            <MainSlider />
         </Container>
     );
 }
