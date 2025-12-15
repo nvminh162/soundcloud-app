a9e2f4196f503d575df6a2b9b467a114af0c15c4
nvminh162 <nvminh162@gmail.com>
2025-12-15 22:26:59 +0700

    feat: enhance WaveTrack component with improved waveform and progress gradients

--- soundcloud-nextjs-frontend/src/app/track/[slug]/page.tsx
+++ soundcloud-nextjs-frontend/src/app/track/[slug]/page.tsx
@@ -10,6 +10,7 @@ export default function DetailTrackPage(props: any) {
 
     return (
         <div>
+            Detail
             <WaveTrack />
         </div>
     );
--- soundcloud-nextjs-frontend/src/components/track/wave.track.tsx
+++ soundcloud-nextjs-frontend/src/components/track/wave.track.tsx
@@ -11,12 +11,32 @@ export default function WaveTrack() {
     const [isPlaying, setIsPlaying] = useState(false);
 
     const optionsMemo = useMemo((): Omit<WaveSurferOptions, 'container'> => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d')!;
+        // Define the waveform gradient
+        const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
+        gradient.addColorStop(0, '#656666') // Top color
+        gradient.addColorStop((canvas.height * 0.7) / canvas.height, '#656666') // Top color
+        gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
+        gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff') // White line
+        gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#B1B1B1') // Bottom color
+        gradient.addColorStop(1, '#B1B1B1') // Bottom color
+        // Define the progress gradient
+        const progressGradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35)
+        progressGradient.addColorStop(0, '#EE772F') // Top color
+        progressGradient.addColorStop((canvas.height * 0.7) / canvas.height, '#EB4926') // Top color
+        progressGradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
+        progressGradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff') // White line
+        progressGradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#F6B094') // Bottom color
+        progressGradient.addColorStop(1, '#F6B094') // Bottom color
+
         return {
-            waveColor: 'rgb(200, 0, 200)',
-            progressColor: 'rgb(100, 0, 100)',
-            barWidth: 2.5,
+            waveColor: gradient,
+            progressColor: progressGradient,
+            height: 150,
+            barWidth: 2,
             url: `/api?audio=${fileName}`,
-        };
+        }
     }, [fileName]);
 
     const wavesurfer = useWavesurfer(containerRef, optionsMemo);
