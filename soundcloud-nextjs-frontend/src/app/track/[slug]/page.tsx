3ec99d3e4995d086612ca7fa354b2e724deb146e
nvminh162 <nvminh162@gmail.com>
2025-12-15 13:59:40 +0700

    refactor: improve MainSlider component structure and styling, add Link for track navigation

---
+++ soundcloud-nextjs-frontend/src/app/track/[slug]/page.tsx
@@ -0,0 +1,4 @@
+export default function DetailTrackPage(props: any) {
+    const { params } = props;
+    return <div>DetailTrackPage: {params.slug}</div>;
+}
--- soundcloud-nextjs-frontend/src/components/main/main.slider.tsx
+++ soundcloud-nextjs-frontend/src/components/main/main.slider.tsx
@@ -1,29 +1,33 @@
-'use client'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
-import { Settings } from "react-slick";
-import { Box } from "@mui/material";
-import Button from "@mui/material/Button/Button";
+'use client';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+import { Settings } from 'react-slick';
+import { Box } from '@mui/material';
+import Button from '@mui/material/Button/Button';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import Divider from '@mui/material/Divider';
+import Link from 'next/link';
 
 interface IProps {
-    title: string
-    data: ITrackTop[]
+    title: string;
+    data: ITrackTop[];
 }
 
 const MainSlider = (props: IProps) => {
-    const { title, data } = props
+    const { title, data } = props;
 
     const NextArrow = (props: any) => {
         return (
-            <Button variant="contained" color="inherit" onClick={props.onClick}
+            <Button
+                variant="contained"
+                color="inherit"
+                onClick={props.onClick}
                 sx={{
-                    position: "absolute",
+                    position: 'absolute',
                     right: 25,
-                    top: "25%",
+                    top: '25%',
                     zIndex: 2,
                     minWidth: 30,
                     width: 35,
@@ -31,15 +35,18 @@ const MainSlider = (props: IProps) => {
             >
                 <ChevronRightIcon />
             </Button>
-        )
-    }
+        );
+    };
 
     const PrevArrow = (props: any) => {
         return (
-            <Button variant="contained" color="inherit" onClick={props.onClick}
+            <Button
+                variant="contained"
+                color="inherit"
+                onClick={props.onClick}
                 sx={{
-                    position: "absolute",
-                    top: "25%",
+                    position: 'absolute',
+                    top: '25%',
                     zIndex: 2,
                     minWidth: 30,
                     width: 35,
@@ -47,8 +54,8 @@ const MainSlider = (props: IProps) => {
             >
                 <ChevronLeftIcon />
             </Button>
-        )
-    }
+        );
+    };
 
     const settings: Settings = {
         infinite: true,
@@ -61,40 +68,41 @@ const MainSlider = (props: IProps) => {
     //box === div
 
     return (
-
         <Box
             sx={{
-                margin: "0 50px",
-                ".track": {
-                    padding: "0 10px",
-                    "img": {
-                        height: 150,
-                        width: 150
-                    }
+                margin: '0 50px',
+                '& a': {
+                    textDecoration: 'none', // Bỏ gạch chân
+                    color: 'inherit',       // Bỏ màu xanh mặc định của Link, dùng màu của text cha
                 },
-                "h3": {
-                    border: "1px solid #ccc",
-                    padding: "20px",
-                    height: "200px",
-
-                }
+                '.track': {
+                    padding: '0 10px',
+                    img: {
+                        height: 150,
+                        width: 150,
+                    },
+                },
+                h3: {
+                    border: '1px solid #ccc',
+                    padding: '20px',
+                    height: '200px',
+                },
             }}
         >
             <h2>{title}</h2>
 
             <Slider {...settings}>
-                {data.map(track => (
-                    <div className="track" key={track._id}>
+                {data.map((track) => (
+                    <Link href={`track/${track._id}`} className="track" key={track._id}>
                         <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/images/${track.imgUrl}`} />
                         <h4>{track.title}</h4>
                         <h5>{track.description}</h5>
-                    </div>
+                    </Link>
                 ))}
             </Slider>
             <Divider />
         </Box>
-
     );
-}
+};
 
 export default MainSlider;
