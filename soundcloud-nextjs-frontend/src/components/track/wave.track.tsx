5f124440b7c365319fb07b129c8bf5fe711f06b6
nvminh162 <nvminh162@gmail.com>
2025-12-16 00:33:21 +0700

    feat: enhance WaveTrack component with play/pause functionality and improved layout

--- soundcloud-nextjs-frontend/src/components/track/wave.track.tsx
+++ soundcloud-nextjs-frontend/src/components/track/wave.track.tsx
@@ -4,6 +4,8 @@ import { useEffect, useRef, useState, useMemo, useCallback } from "react";
 import { useSearchParams } from 'next/navigation';
 import { useWavesurfer } from "@/utils/customHook";
 import { WaveSurferOptions } from 'wavesurfer.js';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
 import './wave.scss';
 
 const WaveTrack = () => {
@@ -43,8 +45,8 @@ const WaveTrack = () => {
         return {
             waveColor: gradient,
             progressColor: progressGradient,
-            height: 150,
-            barWidth: 2,
+            height: 100,
+            barWidth: 3,
             url: `/api?audio=${fileName}`,
         }
     }, []);
@@ -54,8 +56,8 @@ const WaveTrack = () => {
     // Initialize wavesurfer when the container mounts
     // or any of the props change
     useEffect(() => {
-        if (!wavesurfer) return
-        setIsPlaying(false)
+        if (!wavesurfer) return;
+        setIsPlaying(false);
 
         const hover = hoverRef.current!;
         const waveform = containerRef.current!;
@@ -70,6 +72,9 @@ const WaveTrack = () => {
             wavesurfer.on('timeupdate', (currentTime) => {
                 setTime(formatTime(currentTime));
             }),
+            wavesurfer.once('interaction', () => {
+                wavesurfer.play()
+            })
         ]
 
         return () => {
@@ -92,17 +97,108 @@ const WaveTrack = () => {
     }
 
     return (
-        <div style={{ marginTop: 100 }}>
-            <div ref={containerRef} className="wave-form-container">
-                <div className="time" >{time}</div>
-                <div className="duration" >{duration}</div>
-                <div ref={hoverRef} className="hover-wave"></div>
-            </div>
-            <button onClick={() => onPlayClick()}>
-                {isPlaying === true ? "Pause" : "Play"}
-            </button>
-        </div>
+        <div style={{ marginTop: 20 }}>
+            <div
+                style={{
+                    display: "flex",
+                    gap: 15,
+                    padding: 20,
+                    height: 400,
+                    background: "linear-gradient(135deg, rgb(106, 112, 67) 0%, rgb(11, 15, 20) 100%)"
+                }}
+            >
+                <div className="left"
+                    style={{
+                        width: "75%",
+                        height: "calc(100% - 10px)",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "space-between"
+                    }}
+                >
+                    <div className="info" style={{ display: "flex" }}>
+                        <div>
+                            <div
+                                onClick={() => onPlayClick()}
+                                style={{
+                                    borderRadius: "50%",
+                                    background: "#f50",
+                                    height: "50px",
+                                    width: "50px",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    cursor: "pointer"
+                                }}
+                            >
+                                {isPlaying === true ?
+                                    <PauseIcon
+                                        sx={{ fontSize: 30, color: "white" }}
+                                    />
+                                    :
+                                    <PlayArrowIcon
+                                        sx={{ fontSize: 30, color: "white" }}
+                                    />
+                                }
+                            </div>
+                        </div>
+                        <div style={{ marginLeft: 20 }}>
+                            <div style={{
+                                padding: "0 5px",
+                                background: "#333",
+                                fontSize: 30,
+                                width: "fit-content",
+                                color: "white"
+                            }}>
+                                nvminh162's song
+                            </div>
+                            <div style={{
+                                padding: "0 5px",
+                                marginTop: 10,
+                                background: "#333",
+                                fontSize: 20,
+                                width: "fit-content",
+                                color: "white"
+                            }}
+                            >
+                                Van Minh
+                            </div>
+                        </div>
+                    </div>
+                    <div ref={containerRef} className="wave-form-container">
+                        <div className="time" >{time}</div>
+                        <div className="duration" >{duration}</div>
+                        <div ref={hoverRef} className="hover-wave"></div>
+                        <div className="overlay"
+                            style={{
+                                position: "absolute",
+                                height: "30px",
+                                width: "100%",
+                                bottom: "0",
+                                // background: "#ccc"
+                                backdropFilter: "brightness(0.5)"
+                            }}
+                        ></div>
 
+                    </div>
+                </div>
+                <div className="right"
+                    style={{
+                        width: "25%",
+                        padding: 15,
+                        display: "flex",
+                        alignItems: "center"
+                    }}
+                >
+                    <div style={{
+                        background: "#ccc",
+                        width: 250,
+                        height: 250
+                    }}>
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 
