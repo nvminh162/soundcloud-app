9c85abba51983c55c3e4576f1c21238df072d2ca
nvminh162 <nvminh162@gmail.com>
2025-12-17 19:56:11 +0700

    feat: enhance file upload functionality with custom button and improved dropzone instructions

--- soundcloud-fe/soundcloud-nextjs-client/src/components/track/step/step.one.tsx
+++ soundcloud-fe/soundcloud-nextjs-client/src/components/track/step/step.one.tsx
@@ -1,9 +1,45 @@
 'use client';
 import { FileWithPath, useDropzone } from 'react-dropzone';
-import './theme.css'
+import './theme.css';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { useCallback } from 'react';
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
+
+function InputFileUpload() {
+    return (
+        <Button
+            onClick={(e) => e.preventDefault()}
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<CloudUploadIcon />}
+        >
+            Upload files
+            <VisuallyHiddenInput type="file" onChange={(event) => console.log(event.target.files)} multiple />
+        </Button>
+    );
+}
 
 export default function StepOne() {
-    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+    const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+        console.log('acceptedFile: ', acceptedFiles);
+    }, []);
+
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone({ onDrop });
 
     const files = acceptedFiles.map((file: FileWithPath) => (
         <li key={file.path}>
@@ -15,7 +51,8 @@ export default function StepOne() {
         <section className="container">
             <div {...getRootProps({ className: 'dropzone' })}>
                 <input {...getInputProps()} />
-                <p>Drag 'n' drop some files here, or click to select files</p>
+                <InputFileUpload />
+                <p>Click or Drag/Drop here to uploads file track</p>
             </div>
             <aside>
                 <h4>Files</h4>
