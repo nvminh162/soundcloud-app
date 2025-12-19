5eff7d6aab79a74161dae7ac2f2f25bfe4527aec
nvminh162 <nvminh162@gmail.com>
2025-12-19 23:46:40 +0700

    feat: implement user profile page with track display and update AppFooter for improved layout

--- soundcloud-fe/soundcloud-nextjs-client/src/app/(user)/profile/[slug]/page.tsx
+++ soundcloud-fe/soundcloud-nextjs-client/src/app/(user)/profile/[slug]/page.tsx
@@ -1,12 +1,34 @@
-import { sendRequest } from '@/utils/api';
+import ProfileTracks from "@/components/profile/profile.tracks";
+import { sendRequest } from "@/utils/api";
+import { Container } from "@mui/material";
+import Grid from '@mui/material/Grid';
 
-export default async function ProfilePage({ params }: { params: { slug: string } }) {
-    const res = await sendRequest<IBackendRes<ITrackTop[]>>({
-        url: 'http://localhost:8000/api/v1/tracks/users?current=1&pageSize=10',
-        method: 'POST',
-        body: { id: params.slug },
-    });
-    console.log(res);
+const ProfileUserPage = async ({ params }: { params: { slug: string } }) => {
+    const slug = params.slug;
 
-    return <div>{params.slug}</div>;
+    const data = await sendRequest({
+        url: "http://localhost:8000/api/v1/tracks/users?current=1&pageSize=10",
+        method: "POST",
+        body: { id: slug }
+    })
+
+    //@ts-ignore
+    const d = data?.data?.result ?? []
+    return (
+
+        <Container sx={{ my: 5 }}>
+            <Grid container spacing={5}>
+                {d.map((item: any, index: number) => {
+                    return (
+                        <Grid item xs={12} md={6} key={index}>
+                            <ProfileTracks data={item} />
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        </Container>
+
+    )
 }
+
+export default ProfileUserPage;
--- soundcloud-fe/soundcloud-nextjs-client/src/components/footer/app.footer.tsx
+++ soundcloud-fe/soundcloud-nextjs-client/src/components/footer/app.footer.tsx
@@ -10,29 +10,32 @@ export default function AppFooter() {
     if (!hasMounted) return <></>;
 
     return (
-        <AppBar position="fixed" sx={{ top: 'auto', bottom: 0, backgroundColor: '#f2f2f2' }}>
-            <Container sx={{ display: 'flex', gap: 10 }}>
-                <AudioPlayer
-                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/hoidanit.mp3`}
-                    volume={0.5}
-                    style={{
-                        boxShadow: 'unset',
-                        background: '#f2f2f2',
-                    }}
-                />
-                <div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'start',
-                        justifyContent: 'center',
-                        minWidth: 100,
-                    }}
-                >
-                    <div style={{ color: '#ccc' }}>Eric</div>
-                    <div style={{ color: 'black' }}>Who am I ?</div>
-                </div>
-            </Container>
-        </AppBar>
+        <div style={{ marginTop: 50 }}>
+            <AppBar position="fixed" sx={{ top: 'auto', bottom: 0, backgroundColor: '#f2f2f2' }}>
+                <Container sx={{ display: 'flex', gap: 10, '.rhap_main': { gap: '30px' } }}>
+                    <AudioPlayer
+                        layout="horizontal-reverse"
+                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/hoidanit.mp3`}
+                        volume={0.5}
+                        style={{
+                            boxShadow: 'unset',
+                            background: '#f2f2f2',
+                        }}
+                    />
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'start',
+                            justifyContent: 'center',
+                            minWidth: 100,
+                        }}
+                    >
+                        <div style={{ color: '#ccc' }}>Eric</div>
+                        <div style={{ color: 'black' }}>Who am I ?</div>
+                    </div>
+                </Container>
+            </AppBar>
+        </div>
     );
 }
