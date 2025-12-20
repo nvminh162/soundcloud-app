d3af6e1d03e7f3b86f414b971ecea40fb96a3d49
nvminh162 <nvminh162@gmail.com>
2025-12-20 23:56:29 +0700

    feat: enhance audio player functionality in AppFooter and update ProfileTracks for play/pause state management

--- soundcloud-fe/soundcloud-nextjs-client/src/components/footer/app.footer.tsx
+++ soundcloud-fe/soundcloud-nextjs-client/src/components/footer/app.footer.tsx
@@ -2,15 +2,24 @@
 import { useTrackContext } from '@/lib/track.wrapper';
 import { useHasMounted } from '@/utils/customHook';
 import { AppBar, Container } from '@mui/material';
+import { useRef } from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 
 export default function AppFooter() {
     const hasMounted = useHasMounted();
+    const playerRef = useRef(null);
 
     const { currentTrack, setCurrentTrack } = useTrackContext() as ITrackContext;
 
-    console.log(currentTrack);
+    // @ts-ignore
+    if (currentTrack?.isPlaying) {
+        // @ts-ignore
+        playerRef?.current?.audio?.current?.play();
+    } else {
+        // @ts-ignore
+        playerRef?.current?.audio?.current?.pause();
+    }
 
     if (!hasMounted) return <></>;
 
@@ -19,13 +28,16 @@ export default function AppFooter() {
             <AppBar position="fixed" sx={{ top: 'auto', bottom: 0, backgroundColor: '#f2f2f2' }}>
                 <Container sx={{ display: 'flex', gap: 10, '.rhap_main': { gap: '30px' } }}>
                     <AudioPlayer
+                        ref={playerRef}
                         layout="horizontal-reverse"
-                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/hoidanit.mp3`}
+                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/${currentTrack.trackUrl}`}
                         volume={0.5}
                         style={{
                             boxShadow: 'unset',
                             background: '#f2f2f2',
                         }}
+                        onPlay={() => setCurrentTrack({...currentTrack, isPlaying: true})}
+                        onPause={() => setCurrentTrack({...currentTrack, isPlaying: false})}
                     />
                     <div
                         style={{
--- soundcloud-fe/soundcloud-nextjs-client/src/components/profile/profile.tracks.tsx
+++ soundcloud-fe/soundcloud-nextjs-client/src/components/profile/profile.tracks.tsx
@@ -10,8 +10,8 @@ import Typography from '@mui/material/Typography';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
-import { ListItem } from '@mui/material';
 import { useTrackContext } from '@/lib/track.wrapper';
+import PauseIcon from '@mui/icons-material/Pause'
 
 interface IProps {
     data: ITrackTop;
@@ -36,9 +36,29 @@ const ProfileTracks = (props: IProps) => {
                 </CardContent>
                 <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
                     <IconButton aria-label="previous">{theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}</IconButton>
-                    <IconButton aria-label="play/pause" onClick={() => setCurrentTrack({ ...data, isPlaying: false })}>
+                    {/* <IconButton aria-label="play/pause" onClick={() => setCurrentTrack({ ...data, isPlaying: false })}>
                         <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-                    </IconButton>
+                    </IconButton> */}
+                    {(data._id !== currentTrack._id || (data._id === currentTrack._id && currentTrack.isPlaying === false)) && (
+                        <IconButton
+                            aria-label="play/pause"
+                            onClick={(e) => {
+                                setCurrentTrack({ ...data, isPlaying: true });
+                            }}
+                        >
+                            <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                        </IconButton>
+                    )}
+                    {data._id === currentTrack._id && currentTrack.isPlaying === true && (
+                        <IconButton
+                            aria-label="play/pause"
+                            onClick={(e) => {
+                                setCurrentTrack({ ...data, isPlaying: false });
+                            }}
+                        >
+                            <PauseIcon sx={{ height: 38, width: 38 }} />
+                        </IconButton>
+                    )}
                     <IconButton aria-label="next">{theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}</IconButton>
                 </Box>
             </Box>
