8125e0b62b1d633159c411a7e317d185b852aef0
nvminh162 <nvminh162@gmail.com>
2025-12-21 21:01:34 +0700

    feat: add SEO metadata for UserLayout and LikePage components to enhance SEO and page descriptions

--- soundcloud-fe/soundcloud-nextjs-client/src/app/(user)/layout.tsx
+++ soundcloud-fe/soundcloud-nextjs-client/src/app/(user)/layout.tsx
@@ -1,6 +1,13 @@
 import AppFooter from '@/components/footer/app.footer';
 import AppHeader from '@/components/header/app.header';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Tiêu đề from layout',
+    description: 'Miêu tả layout nè',
+};
+
 export default function UserLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
--- soundcloud-fe/soundcloud-nextjs-client/src/app/(user)/like/page.tsx
+++ soundcloud-fe/soundcloud-nextjs-client/src/app/(user)/like/page.tsx
@@ -1,7 +1,10 @@
+import type { Metadata } from 'next';
 
+export const metadata: Metadata = {
+    title: 'nvminh162 like page',
+    description: 'Miêu tả nè',
+};
 
 export default function LikePage() {
-  return (
-    <div>LikePage</div>
-  )
+    return <div>LikePage</div>;
 }
