96748409fd277496ff51b2bab528e0a139763554
nvminh162 <nvminh162@gmail.com>
2025-12-21 23:19:59 +0700

    feat: update track links in MainSlider and ProfileTracks to use slugified titles for improved SEO

--- soundcloud-fe/soundcloud-nextjs-client/src/components/main/main.slider.tsx
+++ soundcloud-fe/soundcloud-nextjs-client/src/components/main/main.slider.tsx
@@ -9,6 +9,7 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import Divider from '@mui/material/Divider';
 import Link from 'next/link';
+import { convertSlugUrl } from '@/utils/api';
 
 interface IProps {
     title: string;
@@ -93,7 +94,7 @@ const MainSlider = (props: IProps) => {
 
             <Slider {...settings}>
                 {data.map((track) => (
-                    <Link href={`track/${track._id}?audio=${track.trackUrl}&id=${track._id}`} className="track" key={track._id}>
+                    <Link href={`track/${convertSlugUrl(track.title)}-${track._id}.html?audio=${track.trackUrl}`} className="track" key={track._id}>
                         <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/images/${track.imgUrl}`} />
                         <h4>{track.title}</h4>
                         <h5>{track.description}</h5>
