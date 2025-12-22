dc4a67195d18710a9a088482c7b3186ae4763d94
nvminh162 <nvminh162@gmail.com>
2025-12-22 14:44:18 +0700

    chore: remove unused routerRefresh image and clean up commented-out code in RootLayout

--- soundcloud-fe/soundcloud-nextjs-client/src/app/layout.tsx
+++ soundcloud-fe/soundcloud-nextjs-client/src/app/layout.tsx
@@ -20,3 +20,26 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </html>
     );
 }
+
+// Loading image too bad
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//     return (
+//         <html lang="en">
+//             <body>
+//                 <img src="/flower/flowers.jpg" alt="123" />
+//             </body>
+//         </html>
+//     );
+// }
+
+// import Image from 'next/image';
+// import flower from '../../public/flower/flowers.jpg';
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//     return (
+//         <html lang="en">
+//             <body>
+//                 <Image src={flower} alt="flower 2" sizes="100vw" style={{ width: '100%', height: 'auto' }} />
+//             </body>
+//         </html>
+//     );
+// }
