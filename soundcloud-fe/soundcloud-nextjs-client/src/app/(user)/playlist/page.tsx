93c193614ee95b9d68972cc611c46f1d63ec3c71
nvminh162 <nvminh162@gmail.com>
2025-12-22 23:54:47 +0700

    feat: add loading delay in PlaylistPage and DetailTrackPage for improved user experience

--- soundcloud-fe/soundcloud-nextjs-client/src/app/(user)/playlist/page.tsx
+++ soundcloud-fe/soundcloud-nextjs-client/src/app/(user)/playlist/page.tsx
@@ -1,6 +1,7 @@
 
 
-export default function PlaylistPage() {
+export default async function PlaylistPage() {
+  await new Promise(resolve => setTimeout(resolve, 3000))
   return (
     <div>PlaylistPage</div>
   )
--- soundcloud-fe/soundcloud-nextjs-client/src/app/(user)/track/[slug]/page.tsx
+++ soundcloud-fe/soundcloud-nextjs-client/src/app/(user)/track/[slug]/page.tsx
@@ -57,6 +57,8 @@ export default async function DetailTrackPage(props: any) {
         },
     });
 
+    await new Promise(resolve => setTimeout(resolve, 3000))
+
     if (!res?.data) {
         notFound();
     }
