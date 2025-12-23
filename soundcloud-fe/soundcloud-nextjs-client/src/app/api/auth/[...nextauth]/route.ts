3d46ecbed47c6a75022cc22d117804e87d8b04b7
nvminh162 <nvminh162@gmail.com>
2025-12-24 01:46:53 +0700

    feat: implement token refresh logic in NextAuth and update environment variables for token expiration settings

--- soundcloud-fe/soundcloud-nextjs-client/src/app/api/auth/[...nextauth]/route.ts
+++ soundcloud-fe/soundcloud-nextjs-client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,43 +1,80 @@
-import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from 'next-auth';
-import { sendRequest } from '@/utils/api';
-import { JWT } from 'next-auth/jwt';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import { sendRequest } from "@/utils/api";
+import { JWT } from "next-auth/jwt";
+import CredentialsProvider from "next-auth/providers/credentials";
+import dayjs from "dayjs";
+
+async function refreshAccessToken(token: JWT) {
+
+    const res = await sendRequest<IBackendRes<JWT>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/refresh`,
+        method: "POST",
+        body: { refresh_token: token?.refresh_token }
+    })
+
+    if (res.data) {
+        console.log(">>> check old token: ", token.access_token);
+        console.log(">>> check new token: ", res.data?.access_token)
+
+        return {
+            ...token,
+            access_token: res.data?.access_token ?? "",
+            refresh_token: res.data?.refresh_token ?? "",
+            access_expire: dayjs(new Date()).add(
+                +(process.env.TOKEN_EXPIRE_NUMBER as string), (process.env.TOKEN_EXPIRE_UNIT as any)
+            ).unix(),
+            error: ""
+        }
+    } else {
+        //failed to refresh token => do nothing
+        return {
+            ...token,
+            error: "RefreshAccessTokenError", // This is used in the front-end, and if present, we can force a re-login, or similar
+        }
+    }
+
+}
 
 export const authOptions: AuthOptions = {
-    secret: process.env.NEXTAUTH_SECRET!,
+    secret: process.env.NEXTAUTH_SECRET,
     // Configure one or more authentication providers
     providers: [
         CredentialsProvider({
             // The name to display on the sign in form (e.g. "Sign in with...")
-            name: 'Soundcloud',
+            name: "nvminh162",
             // `credentials` is used to generate a form on the sign in page.
             // You can specify which fields should be submitted, by adding keys to the `credentials` object.
             // e.g. domain, username, password, 2FA token, etc.
             // You can pass any HTML attribute to the <input> tag through the object.
             credentials: {
-                username: { label: 'Username', type: 'text' },
-                password: { label: 'Password', type: 'password' },
+                username: { label: "Tên đăng nhập", type: "text", },
+                password: { label: "Mật khẩu", type: "password" }
             },
             async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
                 const res = await sendRequest<IBackendRes<JWT>>({
                     url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/login`,
-                    method: 'POST',
-                    body: { username: credentials?.username, password: credentials?.password },
-                });
+                    method: "POST",
+                    body: {
+                        username: credentials?.username,
+                        password: credentials?.password
+                    },
+                })
 
                 if (res && res.data) {
                     // Any object returned will be saved in `user` property of the JWT
                     return res.data as any;
                 } else {
                     // If you return null then an error will be displayed advising the user to check their details.
-                    // return null;
+                    // return null
+                    throw new Error(res?.message as string)
+
                     // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-                    throw new Error(res?.message as string);
                 }
-            },
+            }
         }),
         GithubProvider({
             clientId: process.env.GITHUB_ID!,
@@ -48,35 +85,48 @@ export const authOptions: AuthOptions = {
             clientSecret: process.env.GOOGLE_SECRET!,
         }),
     ],
-    /*
-    15 C14 => 012 1333
-    Mỗi lần login thành công (jwt: ({ token })) sẽ được lưu dưới dạng cookies tại phía browser của client
-    Mỗi lần refresh lại trang, sẽ gọi token trên gửi lên NextServer => phàn token sẽ được giải mã => nạp vào session
-     */
     callbacks: {
         async jwt({ token, user, account, profile, trigger }) {
-            if (trigger === 'signIn' && account?.provider !== 'credentials') {
-                // call api
+            if (trigger === "signIn" && account?.provider !== "credentials") {
                 const res = await sendRequest<IBackendRes<JWT>>({
                     url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/social-media`,
-                    method: 'POST',
-                    body: { type: account?.provider?.toLocaleUpperCase(), username: user.email },
-                });
+                    method: "POST",
+                    body: {
+                        type: account?.provider?.toLocaleUpperCase(),
+                        username: user.email
+                    },
+                })
                 if (res.data) {
-                    token.access_token = res?.data?.access_token;
-                    token.refresh_token = res?.data?.refresh_token;
-                    token.user = res?.data?.user;
+                    token.access_token = res.data?.access_token;
+                    token.refresh_token = res.data.refresh_token;
+                    token.user = res.data.user;
+                    token.access_expire = dayjs(new Date()).add(
+                        +(process.env.TOKEN_EXPIRE_NUMBER as string), (process.env.TOKEN_EXPIRE_UNIT as any)
+                    ).unix(); //.unix(); convert to number
                 }
             }
 
-            if (trigger === 'signIn' && account?.provider === 'credentials') {
-                // @ts-ignore
+            if (trigger === "signIn" && account?.provider === "credentials") {
+                //@ts-ignore
                 token.access_token = user.access_token;
-                // @ts-ignore
+                //@ts-ignore
                 token.refresh_token = user.refresh_token;
-                // @ts-ignore
+                //@ts-ignore
                 token.user = user.user;
+                //@ts-ignore
+                token.access_expire = dayjs(new Date()).add(
+                    +(process.env.TOKEN_EXPIRE_NUMBER as string), (process.env.TOKEN_EXPIRE_UNIT as any)
+
+                ).unix();
+
             }
+
+            const isTimeAfter = dayjs(dayjs(new Date())).isAfter(dayjs.unix((token?.access_expire as number ?? 0)));
+
+            if (isTimeAfter) {
+                return refreshAccessToken(token)
+            }
+
             return token;
         },
         session({ session, token, user }) {
@@ -84,15 +134,14 @@ export const authOptions: AuthOptions = {
                 session.access_token = token.access_token;
                 session.refresh_token = token.refresh_token;
                 session.user = token.user;
+                session.access_expire = token.access_expire;
+                session.error = token.error
             }
             return session;
-        },
-    },
-    // pages: {
-    //     signIn: '/auth/signin',
-    // },
-};
+        }
+    }
+}
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
