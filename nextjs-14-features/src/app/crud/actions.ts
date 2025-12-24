0ebdfc2ece5faa0f6518e3a47516e65dc03b1968
nvminh162 <nvminh162@gmail.com>
2025-12-24 20:16:22 +0700

    feat: add deleteUserAction and integrate it into TableUser component for user deletion functionality

--- nextjs-14-features/src/app/crud/actions.ts
+++ nextjs-14-features/src/app/crud/actions.ts
@@ -37,3 +37,20 @@ export const updateUserAction = async (data: any, access_token: any) => {
     return d;
 }
 
+export const deleteUserAction = async (user: any, access_token: any) => {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    const res = await fetch(
+        `http://localhost:8000/api/v1/users/${user._id}`,
+        {
+            method: "DELETE",
+            headers: {
+                'Authorization': `Bearer ${access_token}`,
+                "Content-Type": "application/json",
+            },
+        })
+
+    const d = await res.json();
+    revalidateTag('listUsers')
+
+    return d;
+}
