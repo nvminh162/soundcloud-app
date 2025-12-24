5b1712f991b4781d691dead50a4dec9a878b54f8
nvminh162 <nvminh162@gmail.com>
2025-12-24 15:01:26 +0700

    feat: implement login form in HomePage for user authentication with server-side handling

--- nextjs-14-features/src/app/page.tsx
+++ nextjs-14-features/src/app/page.tsx
@@ -1,6 +1,26 @@
-
 export default function HomePage() {
+  const handleLogin = async (formData: FormData) => {
+    "use server";
+    console.log(`check form data`, formData.get('password'));
+  };
+
   return (
-    <div>HomePage</div>
-  )
+    <div style={{ marginLeft: 200 }}>
+      <h2>HTML Forms</h2>
+
+      <form action={handleLogin}>
+        <label>Username:</label>
+        <br />
+        <input type="text" name="username" />
+        <br />
+        <br />
+        <label>Password:</label>
+        <br />
+        <input type="text" name="password" />
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
